23c3ebdd0ace81019846b3d48252d379dcff13a7
ayanshuDS <73418468+MET4L-DS@users.noreply.github.com>
2024-04-20 01:13:26 +0530

    added chat functionality, ability to retain chat history, improved the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { useEffect, useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+import { prevHistory } from "./prevHistory";
+
 const App = () => {
     const API_KEY = "AIzaSyAeSRChCXp6BUz8b79p4X34IX9uUzue_xg";
     const genAI = new GoogleGenerativeAI(API_KEY);
 
+    const [prevHistoryText, setPrevHistoryText] = useState("");
+
     const [prompt, setPrompt] = useState("");
     const [text, setText] = useState("");
+    const [history, setHistory] = useState(prevHistory);
+
+    useEffect(() => {
+        setPrevHistoryText(
+            textFormatter(
+                history
+                    .map((message) =>
+                        message.parts.map((part) => part.text).join(" "),
+                    )
+                    .join("\n\n"),
+            ),
+        );
+    }, []);
 
     const delayText = (text) => {
         const textArray = text.split(" ");
@@ -18,49 +35,65 @@ const App = () => {
         });
     };
 
-    const boldTextFormatter = (text) => {
-        const textArray = text.split("**");
-        console.log(textArray);
-        return (newTextArray = textArray.replace(
-            /\*\*(.*?)\*\*/g,
-            "<b>$1</b>",
-        ));
-        // textArray.map((word, _index) => {
-        //     if (_index % 2 === 0) {
-        //         return `<b>${word}</b>`;
-        //     }
-        //     console.log(word);
-        // });
-        // const newTextArray = textArray.map((word, _index) =>
-        //     _index % 2 !== 0 ? `<b>${word}</b>` : word,
-        // );
-        // return newTextArray.join(" ");
+    const textFormatter = (text) => {
+        return text.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
     };
 
-    // useEffect(() => {
-    //     getResponse();
-    // }, []);
+    // not working as expected
+    const convertToListItems = (str) => {
+        const listItemRegex = /^\* (.+)$/gm;
+
+        const listItems = str.replace(listItemRegex, "<li>$1</li>");
+        console.log(`<ul>${listItems}</ul>`);
+        return `<ul>${listItems}</ul>`;
+    };
 
     async function getResponse() {
+        // For text-only input, use the gemini-pro model
         const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-        const result = await model.generateContent(prompt);
+        const chat = model.startChat({
+            history: history,
+        });
+
+        const result = await chat.sendMessage(prompt);
         const response = await result.response;
-        console.log(response.text());
-        const boldText = boldTextFormatter(response.text());
-        delayText(boldText);
-        setPrompt("");
-        // console.log(boldText);
+        console.log(history);
+        setHistory((prevHistory) => [
+            ...prevHistory,
+            { role: "model", parts: [{ text: response.text() }] },
+        ]);
+
+        const formatedText = textFormatter(response.text());
+
+        delayText("\n" + formatedText + "\n");
     }
+
+    const handleInput = () => {
+        setHistory((prevHistory) => [
+            ...prevHistory,
+            { role: "user", parts: [{ text: prompt }] },
+        ]);
+        setText((prevText) => prevText + prompt + "\n");
+        getResponse();
+        setPrompt("");
+    };
+
     return (
         <div className="grid grid-cols-6 gap-4 py-8">
             <div className="col-[2/-2] rounded bg-slate-200">
                 <pre
-                    className="text-wrap p-8"
+                    className="list-disc text-wrap p-8 pb-0"
+                    dangerouslySetInnerHTML={{
+                        __html: prevHistoryText,
+                    }}
+                ></pre>
+                <pre
+                    className="list-disc text-wrap p-8"
                     dangerouslySetInnerHTML={{ __html: text }}
                 ></pre>
             </div>
-            <div className="col-[2/-2] flex gap-4">
+            <div className="sticky bottom-4 col-[2/-2] flex gap-4">
                 <input
                     className="flex-grow rounded border-2 border-purple-500 bg-slate-300 px-4 py-2 font-semibold text-purple-600"
                     type="text"
@@ -72,7 +105,7 @@ const App = () => {
                 <button
                     type="button"
                     className="rounded bg-purple-500 px-4 py-2 hover:bg-purple-600 active:bg-purple-300 "
-                    onClick={() => getResponse()}
+                    onClick={() => handleInput()}
                 >
                     Generate
                 </button>
---
+++ src/prevHistory.js
@@ -0,0 +1,34 @@
+export const prevHistory = [
+    {
+        role: "user",
+        parts: [
+            {
+                text: "what are tanks?",
+            },
+        ],
+    },
+    {
+        role: "model",
+        parts: [
+            {
+                text: "Tanks are armored fighting vehicles that combine firepower, mobility, and protection. They are designed to engage in close-range combat with enemy forces and provide support to infantry units.\n\n**Key Features of Tanks:**\n\n**1. Firepower:**\n* Tanks are equipped with powerful main guns, typically ranging from 75mm to 125mm in caliber.\n* They also carry secondary weapons such as coaxial machine guns, anti-aircraft machine guns, and grenade launchers.\n\n**2. Mobility:**\n* Tanks are designed to be highly mobile and maneuverable on various terrains.\n* They have powerful engines and suspension systems that enable them to navigate rough terrain, cross obstacles, and move at speeds of up to 70 km/h.\n\n**3. Protection:**\n* Tanks are heavily armored with thick plates of steel or composite materials.\n* This armor protects the crew, engine, and other critical components from enemy fire, including armor-piercing rounds, artillery shells, and guided missiles.\n\n**4. Crew:**\n* Tanks typically have a crew of 2-4 members: commander, gunner, loader, and driver.\n* The commander directs the crew and coordinates with other units, while the gunner operates the main gun, the loader handles the ammunition, and the driver controls the tank's movement.\n\n**Types of Tanks:**\n\n**1. Main Battle Tanks (MBTs):**\n* The most common type of tank, designed to provide a balance of firepower, mobility, and protection.\n* Examples: M1 Abrams (USA), Leopard 2 (Germany), T-90 (Russia)\n\n**2. Light Tanks:**\n* Smaller and lighter than MBTs, sacrificing some protection for increased speed and maneuverability.\n* Used for reconnaissance, flank attacks, and support roles.\n* Examples: PT-76 (Russia), FV101 Scorpion (UK)\n\n**3. Heavy Tanks:**\n* Heavily armored and armed, designed for breakthrough operations against fortified enemy positions.\n* Examples: T-34 (Russia), Tiger I (Germany)\n\n**4. Specialized Tanks:**\n* Designed for specific roles, such as anti-aircraft defense, engineering, and urban combat.\n* Examples: Gepard (Germany), Merkava (Israel), M270 Multiple Launch Rocket System (USA)\n\nTanks play a crucial role in modern warfare, providing combined arms capabilities to support infantry, artillery, and air units. Their advanced technology and versatility make them formidable weapons on the battlefield.",
+            },
+        ],
+    },
+    {
+        role: "user",
+        parts: [
+            {
+                text: "which country has the best tanks?",
+            },
+        ],
+    },
+    {
+        parts: [
+            {
+                text: 'Determining which country has the "best" tanks is subjective and depends on various factors, including the specific tank models, their capabilities, and the overall military doctrine of the country. However, some countries are consistently recognized for their advanced tank designs and production capabilities.\n\n**Top Contenders for Best Tanks:**\n\n**1. United States:**\n* Known for producing highly sophisticated and well-rounded tanks, such as the M1 Abrams.\n* The M1 Abrams is considered one of the most powerful and technologically advanced tanks in the world, with a combination of firepower, mobility, and protection.\n\n**2. Germany:**\n* Has a long history of tank development and innovation, producing tanks such as the Leopard 2.\n* The Leopard 2 is renowned for its accuracy, firepower, and advanced electronics, making it a formidable opponent on the battlefield.\n\n**3. Russia:**\n* Inherits the legacy of the Soviet Union\'s tank industry, producing tanks like the T-90 and T-14 Armata.\n* Russian tanks are known for their ruggedness, firepower, and cost-effectiveness.\n\n**4. United Kingdom:**\n* Has a strong tradition of tank design, producing tanks like the Challenger 2.\n* The Challenger 2 is known for its excellent protection, mobility, and firepower, making it a reliable choice for armored warfare.\n\n**5. France:**\n* Has a unique approach to tank design, focusing on mobility and firepower.\n* French tanks, such as the Leclerc, are known for their speed, agility, and powerful main guns.\n\n**Other Notable Countries:**\n\n* **Israel:** Known for developing highly effective tanks tailored to its specific terrain and operational requirements, such as the Merkava.\n* **Japan:** Has a growing tank industry, producing tanks like the Type 10, which combines advanced technology with a compact design.\n* **South Korea:** Has made significant progress in tank development, producing tanks like the K2 Black Panther, known for its firepower, mobility, and advanced electronics.\n\nUltimately, the "best" tank depends on the specific needs and requirements of each country\'s military. However, the countries listed above are consistently recognized for their advanced tank designs and production capabilities.',
+            },
+        ],
+        role: "model",
+    },
+];
