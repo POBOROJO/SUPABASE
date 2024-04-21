1e715fd7f97bc61557782b8ff8802cb3c96a4231
ayanshuDS <73418468+MET4L-DS@users.noreply.github.com>
2024-04-21 15:18:24 +0530

    add image prompt support

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,9 @@ const App = () => {
     const genAI = new GoogleGenerativeAI(API_KEY);
 
     const [prompt, setPrompt] = useState("");
+    const [image, setImage] = useState("");
+    const [inlineImageData, setInlineImageData] = useState({});
     const [history, setHistory] = useState(prevHistory);
-
-    // const delayText = (text) => {
-    //     const textArray = text.split(" ");
-    //     textArray.forEach((word, _index) => {
-    //         const timeout = setTimeout(() => {
-    //             setText((prevText) => prevText + word + " ");
-    //             clearTimeout(timeout);
-    //         }, _index * 50);
-    //     });
-    // };
-
-    // not working as expected
-    const convertToListItems = (str) => {
-        const listItemRegex = /^\* (.+)$/gm;
-
-        const listItems = str.replace(listItemRegex, "<li>$1</li>");
-        console.log(`<ul>${listItems}</ul>`);
-        return `<ul>${listItems}</ul>`;
-    };
 
     const printResponseText = (text) => {
         const textArray = text.split(" ");
@@ -47,6 +30,33 @@ const App = () => {
                 clearTimeout(timeout);
             }, _index * 50);
         });
+    };
+
+    const generateImageParts = (image) => {
+        return {
+            inlineData: {
+                data: Buffer.from(fs.readFileSync(image)).toString("base64"),
+                mimeType: `image/${image.split(".")[2]}`,
+            },
+        };
+    };
+
+    const textAndImagePromptRun = async () => {
+        const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+        // const prompt = "What is the difference between the two images?";
+        const images = [inlineImageData];
+        console.log(prompt);
+        console.log(images);
+
+        const result = await model.generateContent([prompt, ...images]);
+        const response = await result.response;
+        console.log(history);
+
+        setHistory((prevHistory) => [
+            ...prevHistory,
+            { role: "model", parts: [{ text: "" }] },
+        ]);
+        printResponseText(response.text());
     };
 
     async function getResponse() {
@@ -75,27 +85,71 @@ const App = () => {
             { role: "user", parts: [{ text: prompt }] },
         ]);
         // setText((prevText) => prevText + prompt + "\n");
-        getResponse();
+
+        if (image) console.log("image is set");
+        image ? textAndImagePromptRun() : getResponse();
+
+        // getResponse();
+        console.log(image);
+        console.log(inlineImageData);
         setPrompt("");
+        setImage("");
+        setInlineImageData({});
+    };
+
+    const handleFileInput = (file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            setImage(reader.result);
+            setInlineImageData({
+                inlineData: {
+                    data: reader.result.split(",")[1],
+                    mimeType: file.type,
+                },
+            });
+            // console.log(reader.result);
+        };
     };
 
     return (
-        <div className="bg-customNeutral grid grid-cols-7 gap-3  py-8">
+        <div className="grid grid-cols-7 gap-3 bg-customNeutral  py-8">
             <ChatsContainer history={history} />
             <div className="sticky bottom-4 col-[3/-1] flex gap-4 px-8">
                 <input
-                    className="border-customLightGreen flex-grow rounded-2xl border-2 bg-white px-4 py-2 text-base font-semibold shadow-xl"
+                    className="flex-grow rounded-2xl border-2 border-customLightGreen bg-white px-4 py-2 text-base font-semibold shadow-xl"
                     type="text"
                     name="input"
                     id="input"
                     value={prompt}
                     onChange={(e) => setPrompt(e.target.value)}
-                    onKeyDown={(e) => e.key === "Enter" && handleInput()}
+                    onKeyDown={(e) => {
+                        if (!prompt) return;
+                        e.key === "Enter" && handleInput();
+                    }}
+                />
+                <input
+                    type="file"
+                    name="image"
+                    id="image"
+                    accept="image/*"
+                    onChange={(e) => {
+                        console.log(e.target.files[0]);
+                        handleFileInput(e.target.files[0]);
+                    }}
+                />
+                <img
+                    src={image}
+                    alt="image"
+                    className="aspect-square h-8 object-cover"
                 />
                 <button
                     type="button"
-                    className="hover:bg-customLightGreen active:bg-customGreen rounded-2xl bg-white px-4 py-2 shadow-xl "
-                    onClick={() => handleInput()}
+                    className="rounded-2xl bg-white px-4 py-2 shadow-xl hover:bg-customLightGreen active:bg-customGreen "
+                    onClick={() => {
+                        if (!prompt) return;
+                        handleInput();
+                    }}
                 >
                     Generate
                 </button>
